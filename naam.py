17d21dcc1d4bb980f7973dd7eb34e140f6c64817
David Schweizer <david.schweizer@nhlstenden.com>
2022-01-28 12:21:33 +0100

    versie 2

--- naam.py
+++ naam.py
@@ -1,4 +1,4 @@
 class Naam:
-    def __init__(self, achternaam, voornaam):
+    def __init__(self, achternaam='', voornaam=''):
         self.achternaam = achternaam
         self.voornaam = voornaam
